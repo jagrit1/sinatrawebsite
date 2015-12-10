52aa95fd22cbb96ffaabf0bb6c29c5fa6530f921
Jagrit Gyawali <jgyawali@spartaglobal.com>
2015-12-10 18:29:36 +0000

    Switched DB adaptor

--- environments.rb
+++ environments.rb
@@ -1,13 +1,14 @@
 configure :development do
-set :database, 'sqlite3:name.db'
-set :show_exceptions, true
+	set :database, 'sqlite3:name.db'
+	set :show_exceptions, true
 end
 
 configure :production do
  db = URI.parse(ENV['DATABASE_URL'] || 'postgresql:///localhost/mydb')
 
  ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgresql' ? 'postgresql' : db.scheme,
+   # :adapter  => db.scheme == 'postgresql' ? 'postgresql' : db.scheme,
+   :adapter  => 'postgresql',
    :host     => db.host,
    :username => db.user,
    :password => db.password,
